801b6c2cbcae9e58203d50eab35e0ef55d561791
Özenç Geçkin <baranx9@hotmail.com>
2023-11-01 13:02:05 +0300

    Add project files.

---
+++ CoreMVCCodeFirst_1/Controllers/CategoryController.cs
@@ -0,0 +1,89 @@
+﻿using CoreMVCCodeFirst_1.Models.Categories.PageVms;
+using CoreMVCCodeFirst_1.Models.Categories.PureVms;
+using CoreMVCCodeFirst_1.Models.Categories.RequestModels;
+using CoreMVCCodeFirst_1.Models.Categories.ResponseModels;
+using CoreMVCCodeFirst_1.Models.ContextClasses;
+using CoreMVCCodeFirst_1.Models.Entities;
+using CoreMVCCodeFirst_1.Models.MapperClasses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCCodeFirst_1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        MyContext _db;
+
+        public CategoryController(MyContext db)
+        {
+
+            _db = db;
+        }
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryRequestModel category)
+        {
+            //Bir class'ın bilgilerini baska bir class'a aktarmaya Map'leme işlemi denir...Database Domain Entity kabul eder...Dolayısıyla biz aldıgımız CreateCategoryRequestModel icerisindeki bilgileri Category tipinde bir instance alıp o instance'in özellikleri icerisine attık ki database'e Category ekleyebilelim...Cünkü _db.Categories.Add() metodu sadece Category tipinde bir nesne kabul eder... Yani RequestModel'imizi Doman Entity'e mapledik...
+
+
+            Category c = new()
+            {
+                CategoryName = category.CategoryName,
+                Description = category.Description
+            };
+
+            _db.Categories.Add(c);
+            _db.SaveChanges();
+
+            //View metodu, View döndürür...Action'i calıstırmaz...
+
+            return RedirectToAction("GetCategories");
+        }
+
+
+        public IActionResult GetCategories()
+        {
+            List<CategoryResponseModel> categories = _db.Categories.Select(x => new CategoryResponseModel
+            {
+                ID = x.ID,
+                CategoryName = x.CategoryName,
+                Description = x.Description
+            }).ToList();
+
+            CategoryResponsePageVM cpvm = new CategoryResponsePageVM
+            {
+                Categories = categories
+            };
+
+            return View(cpvm);
+        }
+
+        public IActionResult UpdateCategory(int id)
+        {
+
+            CategoryVM category = CategoryMapper.GetCategoryVM(_db.Categories.Find(id));
+
+            CategorySharedPageVM cpVm = new()
+            {
+                Category = category
+            };
+
+            return View(cpVm);
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateCategory()
+        {
+            return View();
+        }
+      
+
+
+
+
+    }
+}
---
+++ CoreMVCCodeFirst_1/Models/Categories/PageVms/CategoryResponsePageVM.cs
@@ -0,0 +1,11 @@
+﻿using CoreMVCCodeFirst_1.Models.Categories.ResponseModels;
+
+namespace CoreMVCCodeFirst_1.Models.Categories.PageVms
+{
+    public class CategoryResponsePageVM
+    {
+        public List<CategoryResponseModel> Categories { get; set; }
+        public CategoryResponseModel Category { get; set; }
+
+    }
+}
